a52056d695e78e120a585e7c282bb2f711bab538
Nastya <asekerich99@gmail.com>
2020-10-19 11:42:57 +0300

    feat: add decode function

--- src/index.js
+++ src/index.js
@@ -37,9 +37,41 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function getLetterFromMorse(code) {
+  if ((code === "**********")) {
+    return " ";
+  }
+
+  for (let i=0; i<code.length; i++){
+    if (code[i] === '0' && code[i+1] === '1') {
+      code=code.slice(i+1);
+    } break;
+  }
+
+  let result = '';
+  for (let i=0; i<code.length; i+=2) {
+    if (code[i] + code[i+1] === '10') {
+      result +='.';
+    } else if (code[i] + code[i+1] === '11') {
+      result +='-';
+    }
+  }
+
+  let value = MORSE_TABLE[result];
+  return value;
 }
+
+
+function decode(expr) {
+  let result = '';
+  for (let i=10; i<expr.length+1; i+=10) {
+    let value = getLetterFromMorse (expr.slice(i-10, i));
+    result += value;
+  }
+  return result;
+}
+
+
 
 module.exports = {
     decode
